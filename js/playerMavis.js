1e0352527687f30a4e198c126d23f8a4102693dd
isabellaoliv <visabella303@yahoo.com>
2022-05-03 13:03:04 -0300

    update

---
+++ js/playerMavis.js
@@ -0,0 +1,56 @@
+function decisao1( ) {
+    alert ( 'Escolha um número: 1 ou 2');
+    var escolha1 = prompt ('Qual é a opção correta?');
+    while (true) {
+        if (escolha1 == 1){
+            window.location = ("../html/gamehover-loser.html");
+            break;
+        }
+        else if (escolha1 == 2) {
+            window.location = ("../html/playerMavis2.html");
+            break;
+        }
+        else {
+            alert ('Opção inválida!');
+            escolha1 = prompt ('Escolha entre os números 1 ou 2 para seguir na próxima fase:');
+        }
+    }
+}
+
+function decisao2( ) {
+    alert ( 'Escolha um número: 1 ou 2');
+    var escolha1 = prompt ('Qual é a opção correta?');
+    while (true) {
+        if (escolha1 == 1){
+            window.location = ("../html/playerMavis3.html");
+            break;
+        }
+        else if (escolha1 == 2) {
+            window.location = ("../html/gamehover-loser.html");
+            break;
+        }
+        else {
+            alert ('Opção inválida!');
+            escolha1 = prompt ('Escolha entre os números 1 ou 2 para seguir na próxima fase:');
+        }
+    }
+}
+
+function decisao3( ) {
+    alert ( 'Escolha um número: 1 ou 2');
+    var escolha1 = prompt ('Qual é a opção correta?');
+    while (true) {
+        if (escolha1 == 1){
+            window.location = ("../html/gamehover-loser.html");
+            break;
+        }
+        else if (escolha1 == 2) {
+            window.location = ("../html/gamehover-winner.html");
+            break;
+        }
+        else {
+            alert ('Opção inválida!');
+            escolha1 = prompt ('Escolha entre os números 1 ou 2 para seguir na próxima fase:');
+        }
+    }
+}
